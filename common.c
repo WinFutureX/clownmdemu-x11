b3e5d3ea54cedfbcdaf97ebfedc1c6c003be7a46
Kelsey Boey <winfuturex@gmail.com>
2025-10-01 21:37:57 +0800

    common: throw away `__unix__` define on linux

--- common.c
+++ common.c
@@ -1,6 +1,9 @@
-/* for compatibility with musl libc */
+/* 
+ * some linux distros define __unix__ alongside __linux__ 
+ * we'll throw away the __unix__ define so we can force the use of setjmp instead of sigsetjmp
+ */
 #if defined(__unix__) && defined(__linux__)
-#define sigjmp_buf jmp_buf
+#undef __unix__
 #endif
 
 #include "common/unity.c"
