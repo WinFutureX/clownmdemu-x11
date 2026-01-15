26f484fbdc76c422566de9252452acb4af704617
Kelsey Boey <winfuturex@gmail.com>
2026-01-16 05:52:18 +0800

    emulator: fix crash when writing save ram for files without extensions

--- main.c
+++ main.c
@@ -329,8 +329,12 @@ char * strip_ext(const char * filename)
 			memcpy(ret, copy, diff);
 			ret[diff] = '\0';
 		}
+		free(copy);
 	}
-	free(copy);
+	else
+	{
+		ret = copy;
+	}
 	return ret;
 }
 
@@ -889,6 +893,7 @@ void emulator_iterate(emulator * emu)
 
 int emulator_load_file(emulator * emu, const char * filename)
 {
+	char * tmp;
 	if (!file_exists(filename))
 	{
 		printf("emulator_load_file: %s does not exist\n", filename);
@@ -905,7 +910,13 @@ int emulator_load_file(emulator * emu, const char * filename)
 	{
 		if (CDReader_IsMegaCDGame(&emu->cd))
 		{
+			tmp = strdup(filename);
 			emu->cd_boot = cc_true;
+			if (tmp)
+			{
+				emu->cd_filename = strdup(basename(tmp));
+				free(tmp);
+			}
 			printf("booting cd\n");
 			return 1;
 		}
@@ -986,19 +997,12 @@ void emulator_unload_cartridge(emulator * emu)
 
 int emulator_load_cd(emulator * emu, const char * filename)
 {
-	char * tmp;
 	CDReader_Open(&emu->cd, NULL, filename, &emu->cd_callbacks);
 	if (!CDReader_IsOpen(&emu->cd))
 	{
 		return 0;
 	}
-	tmp = strdup(filename);
 	CDReader_SeekToSector(&emu->cd, 0);
-	if (tmp)
-	{
-		emu->cd_filename = strdup(basename(tmp));
-		free(tmp);
-	}
 	return 1;
 }
 
